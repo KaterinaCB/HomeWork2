d7ae9a0072df5692aec7eefba1d56e8158b65e7f
ceb <vivu1991@mail.ru>
2022-08-26 12:54:35 +0300

    DZ 001 решение первой задачи

---
+++ DZ001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите трехзначное число ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+void Example()
+{
+   int x1 = (x / 10) % 10;
+   Console.WriteLine($"{x1}"); 
+}
+Example();

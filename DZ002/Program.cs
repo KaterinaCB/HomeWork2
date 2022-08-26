348d24bfafd37ec444d034332af651ecbc2a2f94
ceb <vivu1991@mail.ru>
2022-08-26 18:24:33 +0300

    DZ 002 решение второй задачи

---
+++ DZ002/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+void ExampleTwo()
+{
+        int x3 = (int)Math.Log10(x)-2;
+    if (x < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else if (x > 100)
+    {
+        x = (x % (int)Math.Pow(10, x3 + 1) / (int)Math.Pow(10, x3));
+        Console.WriteLine($"Третья цифра -> {x}");
+    }
+}
+ExampleTwo();

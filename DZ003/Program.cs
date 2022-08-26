74a65521f266dfebc6e9b9412a12f0ccd00303da
ceb <vivu1991@mail.ru>
2022-08-26 18:46:23 +0300

    DZ 003 решение третьей задачи

---
+++ DZ003/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void ExampleThree()
+{
+    if (num == 6 || num == 7)
+    {
+        Console.WriteLine("Да");
+    }
+    else if (num < 1 || num > 7)
+    {
+        Console.WriteLine("Номер не соответствует продолжительности недели");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+ExampleThree();

d57a4dd2d996042a742ba431e91b75c88323961a
ceb <vivu1991@mail.ru>
2022-08-26 20:10:04 +0300

    DZ 004 решение четвертой задачи

---
+++ DZ004/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите количество: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+void ExampleFour()
+{
+    if (x % 10 == 1 & x % 100 != 11)
+    {
+        Console.WriteLine($"{x} программист");
+    }
+    else if (x % 10 == 2 & x % 100 != 12 || x % 10 == 3 & x % 100 != 13 || x % 10 == 4 & x % 100 != 14)
+    {
+        Console.WriteLine($"{x} программиста");
+    }
+    else 
+    {
+        Console.WriteLine($"{x} программистов");
+    }
+}
+ExampleFour();
